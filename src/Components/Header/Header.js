b947ce0becc4d9bdcd707139ea355ecf9b93b815
floresjmjr <floresjmjr@gmail.com>
2023-07-20 13:57:21 -0500

    feat: added the ability to obtain country information from the header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,16 @@
-const Header = () => {
-  return <p>Header</p>;
+import { useState, useRef } from 'react';
+
+const Header = (props) => {
+  const inputRef = useRef();
+
+  return (
+    <div className='header'>
+      <input type='text' placeholder='search box' ref={inputRef}></input>
+      <button type='button' onClick={() => props.lookupFlagHandler(inputRef.current.value)}>
+        submit
+      </button>
+    </div>
+  );
 };
 
 export default Header;
--- src/Components/WebPage/WebPage.js
+++ src/Components/WebPage/WebPage.js
@@ -4,10 +4,15 @@ import MainSection from '../MainSection/MainSection';
 import SideSection from '../SideSection/SideSection';
 import './WebPage.css';
 
+const lookupFlagHandler = async (country) => {
+  const rawData = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+  const results = await rawData.json();
+};
+
 const WebPage = () => {
   return (
     <React.Fragment>
-      <Header />
+      <Header lookupFlagHandler={lookupFlagHandler} />
       <div className='sections'>
         <MainSection />
         <SideSection />
