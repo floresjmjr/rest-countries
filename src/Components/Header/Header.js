2894b11d3bc5c78264b5c20d4be2565b04d46363
floresjmjr <floresjmjr@gmail.com>
2023-07-20 16:11:59 -0500

    refactor: search submission to be a form and seperate logic

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,12 +3,18 @@ import { useRef } from 'react';
 const Header = (props) => {
   const inputRef = useRef();
 
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    props.lookupFlagHandler(inputRef.current.value);
+    inputRef.current.value = '';
+  };
+
   return (
     <div className='header'>
-      <input type='text' placeholder='search box' ref={inputRef}></input>
-      <button type='button' onClick={() => props.lookupFlagHandler(inputRef.current.value)}>
-        submit
-      </button>
+      <form onSubmit={onSubmitHandler}>
+        <input type='text' placeholder='search box' ref={inputRef}></input>
+        <button type='submit'>submit</button>
+      </form>
     </div>
   );
 };
