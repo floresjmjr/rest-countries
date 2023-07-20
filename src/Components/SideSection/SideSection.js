33c666eb82e89f5d1d8d92c737a485fc88e3c3c2
floresjmjr <floresjmjr@gmail.com>
2023-07-20 14:34:24 -0500

    feat: side section renders flag and coat of arms upon submission

--- src/Components/SideSection/SideSection.js
+++ src/Components/SideSection/SideSection.js
@@ -1,5 +1,18 @@
-const SideSection = () => {
-  return <p>SideSection</p>;
+import React from 'react';
+
+const SideSection = (props) => {
+  return (
+    <React.Fragment>
+      {props.flag ? (
+        <div>
+          <img src={props.flag} alt={props.flagInfo} />
+          <img src={props.coatOfArms} alt='coat of arms' />
+        </div>
+      ) : (
+        <div>Country flag and coat of arms panel</div>
+      )}
+    </React.Fragment>
+  );
 };
 
 export default SideSection;
--- src/Components/WebPage/WebPage.js
+++ src/Components/WebPage/WebPage.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../Header/Header';
 import MainSection from '../MainSection/MainSection';
 import SideSection from '../SideSection/SideSection';
 import './WebPage.css';
 
-const lookupFlagHandler = async (country) => {
-  const rawData = await fetch(`https://restcountries.com/v3.1/name/${country}`);
-  const results = await rawData.json();
-};
-
 const WebPage = () => {
+  const [countryInfo, setCountryInfo] = useState({
+    commoneName: '',
+    officialName: '',
+    currencySymbol: '',
+    language: '',
+    capitalCity: '',
+    population: '',
+    flag: '',
+    flagInfo: '',
+    coatOfArms: '',
+  });
+
+  const lookupFlagHandler = async (country) => {
+    const rawData = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+    const results = await rawData.json();
+    setCountryInfo({
+      commoneName: results[0].name.common,
+      officialName: results[0].name.official,
+      currencySymbol: results[0].currencies, //iterate
+      language: results[0].languages, //iterate
+      capitalCity: results[0].capital[0],
+      population: results[0].population,
+      flag: results[0].flags.svg,
+      flagInfo: results[0].flags.alt,
+      coatOfArms: results[0].coatOfArms.svg,
+    });
+  };
+
   return (
     <React.Fragment>
       <Header lookupFlagHandler={lookupFlagHandler} />
       <div className='sections'>
         <MainSection />
-        <SideSection />
+        <SideSection flag={countryInfo.flag} coatOfArms={countryInfo.coatOfArms} />
       </div>
     </React.Fragment>
   );
